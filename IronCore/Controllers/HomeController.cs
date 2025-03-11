034093c86b85d90c8be09cf7f213bbb529f582f7
Vitalii Liogchi <legkiy1002@gmail.com>
2025-03-12 01:55:43 +0200

    Add project files.

---
+++ IronCore/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IronCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Why()
+        {
+            return View();
+        }
+
+        public ActionResult Programs()
+        {
+            return View();
+        }
+
+        public ActionResult Nutritions()
+        {
+            return View();
+        }
+
+        public ActionResult Trainers()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ IronCore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IronCore.MvcApplication" Language="C#" %>
