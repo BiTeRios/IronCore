e53820bcf9207e8f6143d1282373900f0309d6c6
Vitalii Liogchi <legkiy1002@gmail.com>
2025-03-16 21:28:08 +0200

    Added some pages

--- IronCore/Controllers/HomeController.cs
+++ IronCore/Controllers/HomeController.cs
@@ -10,34 +10,48 @@ namespace IronCore.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.ActivePage = "Index";
             return View();
         }
-
         public ActionResult Why()
         {
+            ViewBag.ActivePage = "Why";
             return View();
         }
-
         public ActionResult Programs()
         {
+            ViewBag.ActivePage = "Programs";
             return View();
         }
-
         public ActionResult Nutritions()
         {
+            ViewBag.ActivePage = "Nutritions";
             return View();
         }
-
         public ActionResult Trainers()
         {
+            ViewBag.ActivePage = "Trainers";
             return View();
         }
-
         public ActionResult Contact()
         {
+            ViewBag.ActivePage = "Contact";
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.ActivePage = "Login";
             return View();
         }
         public ActionResult Page404()
+        {
+            return View();
+        }
+        public ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult ProgramDetail()
         {
             return View();
         }
@@ -46,6 +60,10 @@ namespace IronCore.Controllers
             return View();
         }
         public ActionResult Coach()
+        {
+            return View();
+        }
+        public ActionResult ForgotPass()
         {
             return View();
         }
