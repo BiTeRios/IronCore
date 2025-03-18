7875e69fdf98b85f30b0428e90984d9daac90934
Meliuh Samir <samrealka@mail.ru>
2025-03-18 21:18:43 +0200

    Trainer model, ShoppingCart model

---
+++ IronCore/Models/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IronCore.Models
+{
+	public class Program
+	{
+        [Key]
+        public int ProgramId { get; set; }
+
+        [Required(ErrorMessage = "Введите название программы")]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Введите краткое описание программы")]
+        [StringLength(500)]
+        public string ShortDescription { get; set; }
+
+        public string FullDescription { get; set; }
+
+        [Range(0, 100000, ErrorMessage = "Цена не может быть отрицательной")]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ IronCore/Models/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IronCore.Models
+{
+	public class ShoppingCart
+	{
+        [Key]
+        public int CartId { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public virtual ICollection<CartItem> Items { get; set; }
+
+    }
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+
+        public int CartId { get; set; }
+
+        public int ProgramId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
