53dac3b6aaf33896ee34e1dbc1f078e330d59b09
Meliuh Samir <samrealka@mail.ru>
2025-03-19 19:15:07 +0200

    Coach entity

--- IronCore.Domain/Entities/Coach/Coach.cs
+++ IronCore.Domain/Entities/Coach/Coach.cs
@@ -8,6 +8,16 @@ namespace IronCore.Domain.Entities.Coach
 {
     class Coach
     {
-        public int CoachID { get; set; } 
+        public int CoachID { get; set; }
+        public string ImagePath { get; set; }
+        public string FullName { get; set; }
+        public int ExpirienceTime { get; set; }
+        public string Qualification { get; set; }
+        public string Specialization { get; set; }
+        public string Bio { get; set; }
+        public string Testimonials { get; set; }
+        public string TelegramUrl { get; set; }
+        public string SteamUrl { get; set; }
+        public string InstagramUrl { get; set; }
     }
 }
