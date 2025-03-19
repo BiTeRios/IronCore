5c44f0e1f334d2cf7d8dea7ffe0d76440c884507
Meliuh Samir <samrealka@mail.ru>
2025-03-19 20:05:03 +0200

    Small corrections

--- IronCore.Domain/Entities/Coach/Coach.cs
+++ IronCore.Domain/Entities/Coach/Coach.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace IronCore.Domain.Entities.Coach
 {
     class Coach
     {
+
         public int CoachID { get; set; }
         public string ImagePath { get; set; }
         public string FullName { get; set; }
-        public int ExpirienceTime { get; set; }
+        public int ExperienceTime { get; set; }
         public string Qualification { get; set; }
         public string Specialization { get; set; }
         public string Bio { get; set; }
@@ -19,6 +21,6 @@ namespace IronCore.Domain.Entities.Coach
         public string TelegramUrl { get; set; }
         public string SteamUrl { get; set; }
         public string InstagramUrl { get; set; }
-        public DateTime RegistrationDate { get; set; }
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
     }
 }
