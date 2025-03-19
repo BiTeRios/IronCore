63a841817cb448c28dcfff6492b32cdaef23114b
Vitalii Liogchi <legkiy1002@gmail.com>
2025-03-19 19:14:45 +0200

    Product changes

--- IronCore.Domain/Entities/Product/Product.cs
+++ IronCore.Domain/Entities/Product/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,13 @@ namespace IronCore.Domain.Entities.Product
     {
         public int ProductID { get; set; }
         public string ProductName { get; set; }
-        public DateTime DateOfCreation { get; set; }
         public string Description { get; set; }
+        public int Price { get; set; }
+        public bool InCart { get; set; }
+        public int Quantity { get; set; }
+        public string ImagePath { get; set; }
+        public string BrandOrTrainer { get; set; }
+        public string ShortDescription { get; set; }
+        public DateTime DateOfCreation { get; set; }
     }
 }
