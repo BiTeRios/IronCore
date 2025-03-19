458bdf6eb14ccee52ea2a104bd8b3d8d997b0e05
Vitalii Liogchi <legkiy1002@gmail.com>
2025-03-19 20:48:43 +0200

    Some changes

---
+++ IronCore.BusinessLogic/Interfaces/IProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronCore.Domain.Entities.Product;
+
+namespace IronCore.BusinessLogic.Interfaces
+{
+    interface IProduct
+    {
+        ProductItem GetProductById(int productId);
+        IEnumerable<ProductItem> GetAllProducts();
+        void CreateProduct(ProductItem product);
+        bool DeleteProduct(int productId);
+        void UpdateProduct(ProductItem product);
+    }
+}
