98e8f11410bee1c572c44bbbf5f3cb98c307337d
Meliuh Samir <samrealka@mail.ru>
2025-03-20 18:22:03 +0200

    UserBL, CoachBL

---
+++ IronCore.BusinessLogic/BL/CoachBL.cs
@@ -0,0 +1,34 @@
+﻿using IronCore.BusinessLogic.Interfaces;
+using IronCore.Domain.Entities.Coach;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronCore.BusinessLogic.Core;
+
+namespace IronCore.BusinessLogic.BL
+{
+    class CoachBL : UserApi, ICoach
+    {
+        public void addCoach(CoachCl coach)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteCoach(int CoachID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CoachCl getInfoAboutCoach(int CoachID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void modifyCoach(CoachCl coach)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IronCore.BusinessLogic/BL/UserBL.cs
@@ -0,0 +1,54 @@
+﻿using IronCore.BusinessLogic.Core;
+using IronCore.BusinessLogic.Interfaces;
+using IronCore.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronCore.BusinessLogic.BL
+{
+    class UserBL : UserApi, IUser
+    {
+        public bool AddUser(UserCl user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeactivateUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserCl> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserCl GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUser(UserCl user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExists(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateCredentials(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
