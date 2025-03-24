07968ed0095aa06c7043a6c8e0bb71527441556d
Meliuh Samir <samrealka@mail.ru>
2025-03-24 18:00:37 +0200

    Entity Framework

---
+++ IronCore.BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronCore.BusinessLogic.DBModel
+{
+    class UserContext : DbContext
+    {
+        public UserContext() :
+            base("name=IronCore")
+        {
+        }
+        public virtual DbSet<UDbTable> Users { get; set; }
+    }
+}
