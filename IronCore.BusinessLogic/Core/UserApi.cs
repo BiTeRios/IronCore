82f8bbe36d15b8cfcec7a0e154e735e324b1cdb8
Vitalii Liogchi <legkiy1002@gmail.com>
2025-04-01 23:21:57 +0300

    Little changes

--- IronCore.BusinessLogic/Core/UserApi.cs
+++ IronCore.BusinessLogic/Core/UserApi.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using IronCore.Domain.Entities.User
+using IronCore.Domain.Entities.User;
 
 namespace IronCore.BusinessLogic.Core
 {
