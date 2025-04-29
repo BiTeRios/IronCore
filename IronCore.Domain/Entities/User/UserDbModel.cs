d59cfdb3de2f9ec9aaef08444e315ab0274cac7e
Vitalii Liogchi <legkiy1002@gmail.com>
2025-04-29 20:40:52 +0300

    web.config

--- IronCore.Domain/Entities/User/UserDbModel.cs
+++ IronCore.Domain/Entities/User/UserDbModel.cs
@@ -31,18 +31,25 @@ namespace IronCore.Domain.Entities.User
         [StringLength(50, MinimumLength = 5, ErrorMessage = "email not valid")]
         public string Email { get; set; }
 
-        public URole Role { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime LastLogin { get; set; }
 
-
-
+        public URole Level { get; set; }
 
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
         public string PhoneNumber { get; set; }
+
         public bool IsEmailConfirmed { get; set; } = false;
+
+        [DataType(DataType.Date)]
         public DateTime RegistrationDate { get; set; } = DateTime.Now;
-        public DateTime LastLoginDate { get; set; }
+
         public bool IsActive { get; set; } = true;
     }
 }
