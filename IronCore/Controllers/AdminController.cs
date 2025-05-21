0822e021ee69b08ecdf8a41ed2e2cd51b9803a11
Meliuh Samir <samrealka@mail.ru>
2025-05-21 12:59:38 +0300

    Admin litlle changes

--- IronCore/Controllers/AdminController.cs
+++ IronCore/Controllers/AdminController.cs
@@ -10,10 +10,31 @@ namespace IronCore.Controllers
 
         public ActionResult Index()
         {
+            ViewBag.ActivePage = "Index";
             return View();                   // Views/Admin/Index.cshtml
         }
 
         [AdminMod]
-        public ActionResult Trainers() => View();
+        public ActionResult Trainers()
+        {
+            ViewBag.ActivePage = "Trainers";
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            ViewBag.ActivePage = "Products";
+            return View();                   
+        }
+        public ActionResult Users()
+        {
+            ViewBag.ActivePage = "Users";
+            return View();                   
+        }
+        public ActionResult Orders()
+        {
+            ViewBag.ActivePage = "Orders";
+            return View();                   
+        }
     }
 }
