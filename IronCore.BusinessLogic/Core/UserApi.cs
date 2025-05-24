475b2ddaecb05eee0798d1e8f3135647827529f5
Meliuh Samir <samrealka@mail.ru>
2025-05-24 20:21:52 +0300

    Added to UserApi Registration and Login

--- IronCore.BusinessLogic/Core/UserApi.cs
+++ IronCore.BusinessLogic/Core/UserApi.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using IronCore.Domain.Entities.User;
+using IronCore.Domain.Entities.User.Login;
 using IronCore.BusinessLogic.DBModel;
+using IronCore.Domain.Enums.User;
+using System.Security.Cryptography;
+using System.Text;
+using IronCore.Domain.Entities.User.Registration;
 
 namespace IronCore.BusinessLogic.Core
 {
@@ -41,7 +46,6 @@ namespace IronCore.BusinessLogic.Core
                 return false;
             }
         }
-
         public bool DeleteAPI(int id)
         {
             try
@@ -60,21 +64,122 @@ namespace IronCore.BusinessLogic.Core
                 return false;
             }
         }
-
-        public bool ExistsAPI(int id)
+        public UserLoginResult LoginAPI(UserLoginData data)
         {
-            using (var db = new UserContext())
+            try
             {
-                return db.Users.Any(u => u.Id == id);
+                using (var db = new UserContext())
+                {
+                    var user = db.Users
+                                 .FirstOrDefault(u =>
+                                     u.Credential == data.Credential ||
+                                     u.Email == data.Credential);
+
+                    if (user == null)
+                        return new UserLoginResult
+                        {
+                            Status = false,
+                            Message = "UserNotFound"
+                        };
+
+                    var passwordHash = HashPassword(data.Password);
+                    if (user.Password != passwordHash)
+                        return new UserLoginResult
+                        {
+                            Status = false,
+                            Message = "PasswordIsWrong"
+                        };
+
+                    user.LastLogin = DateTime.UtcNow;
+                    return new UserLoginResult
+                    {
+                        Status = true,
+                        Message = "UserExist",
+                        UserDTO = new UserDTO
+                        {
+                            Id = user.Id,
+                            Credential = user.Credential,
+                            UserName = user.UserName,
+                            Password = user.Password,
+                            Email = user.Email,
+                            LastLogin = user.LastLogin,
+                            Level = user.Level,
+                            FirstName = user.FirstName,
+                            LastName = user.LastName,
+                            BirthDate = user.BirthDate,
+                            PhoneNumber = user.PhoneNumber,
+                            RegistrationDate = user.RegistrationDate,
+                            Balance = user.Balance
+                        }
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new UserLoginResult
+                {
+                    Status = false,
+                    Message = ex.Message
+                };
             }
         }
-        public UserDbModel Login(string credential, string password)
+        public UserRegistrationResult RegisterAPI(UserRegistrationData data)
         {
-            using (var db = new UserContext())
+            try
             {
-                return db.Users.FirstOrDefault(u =>
-                    (u.Credential == credential || u.UserName == credential) &&
-                    u.Password == password);
+                using (var db = new UserContext())
+                {
+                    if (db.Users.Any(u => u.Credential == data.Credential))
+                        return new UserRegistrationResult
+                        {
+                            Status = false,
+                            Message = "SuchCredentialsIsExist"
+                        };
+
+                    var user = new UserDbModel
+                    {
+                        Credential = data.Credential,
+                        UserName = data.UserName,
+                        Password = HashPassword(data.Password),
+                        Email = data.Email,
+                        Level = URole.User,
+                        FirstName = data.FirstName,
+                        LastName = data.LastName,
+                        BirthDate = data.BirthDate,
+                        PhoneNumber = data.PhoneNumber,
+                        RegistrationDate = DateTime.UtcNow,
+                        Balance = 0
+                    };
+
+                    db.Users.Add(user);
+                    db.SaveChanges();
+
+                    return new UserRegistrationResult
+                    {
+                        Status = true,
+                        Message = "Successful",
+                        User = user
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new UserRegistrationResult
+                {
+                    Status = false,
+                    Message = ex.Message,
+                    User = null
+                };
+            }
+        }
+
+        private static string HashPassword(string password)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(password);
+                var hash = sha.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
             }
         }
     }
--- IronCore.Domain/Entities/User/Login/UserLoginData.cs
+++ IronCore.Domain/Entities/User/Login/UserLoginData.cs
@@ -5,13 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IronCore.Domain.Entities.User
+namespace IronCore.Domain.Entities.User.Login
 {
-    public class ULoginData
+    public class UserLoginData
     {
+        [Required] 
         public string Credential { get; set; }
+        [Required] 
         public string Password { get; set; }
-        public string LoginIp { get; set; }
-        public DateTime LoginDateTime { get; set; }
     }
 }
